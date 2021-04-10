95d5f24500e0af17408e9827aff4db23daab3b60
Angel Flores <angelfloreschicago@gmail.com>
2021-04-10 15:30:46 -0500

    updated html js css and readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ## HW5 Work Day Scheduler
 
 Link: https://angelfloreschicago.github.io/hw5_WorkDayScheduler/
+<img src="./images/schedule_screeshot.png">
 
-## Pseudo Code
-1. First link up css and JS files.
-2. Start by adding the current date to the header.
-3. Add a table to the body.
+## Page Guide/Description
+- The purpose of this website is to help the user schedule his day.
+- The user can type in a task per hour between 9am and 5pm.
+- As the day goes by the scheduler will change colors depending on what
+  time of the day it is.
+- The task typed by the user will get saved to local storage and will be
+  displayed for the user even when the page is refreshed. 
+
+Notes
+- The website still needs some bugs worked out. It mostly has to do with fixing
+  the colors depending on the time of day. This would probably be fixed with an
+  if statement and working with moment.js. Advice is always appreciated! Thanks!
--- js/script.js
+++ js/script.js
@@ -1,78 +1,117 @@
-// var taskId = document.querySelector('#input9');
-///var savePrompt = $('#saveAlert');
-
 var today = moment();
 $("#currentDay").text(today.format("dddd MMM Do, YYYY"));
 
+// Displays last task saved on time blocks
+lastRegisteredTasks();
+function lastRegisteredTasks() {
+savedtask.textContent = localStorage.getItem('task');
+savedtask10.textContent = localStorage.getItem('task1');
+savedtask11.textContent = localStorage.getItem('task2');
+savedtask12.textContent = localStorage.getItem('task3');
+savedtask1.textContent = localStorage.getItem('task4');
+savedtask2.textContent = localStorage.getItem('task5');
+savedtask3.textContent = localStorage.getItem('task6');
+savedtask4.textContent = localStorage.getItem('task7');
+savedtask5.textContent = localStorage.getItem('task8');
+}
 
+//ON click events for 9 different buttons
+//9am
 $('#btn9').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input9').value;
     localStorage.setItem('task', task);
     var taskId = document.querySelector('#input9').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 9am');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//10am
 $('#btn10').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input10').value;
     localStorage.setItem('task1', task);
     var taskId = document.querySelector('#input10').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 10am');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//11am
 $('#btn11').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input11').value;
     localStorage.setItem('task2', task);
     var taskId = document.querySelector('#input11').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 11am');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//12pm
 $('#btn12').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input12').value;
     localStorage.setItem('task3', task);
     var taskId = document.querySelector('#input12').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 12pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//1pm
 $('#btn1').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input1').value;
     localStorage.setItem('task4', task);
     var taskId = document.querySelector('#input1').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 1pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//2pm
 $('#btn2').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input2').value;
     localStorage.setItem('task5', task);
     var taskId = document.querySelector('#input2').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 2pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//3pm
 $('#btn3').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input3').value;
     localStorage.setItem('task6', task);
     var taskId = document.querySelector('#input3').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 3pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
+//4pm
 $('#btn4').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     var task = document.querySelector('#input4').value;
     localStorage.setItem('task7', task);
     var taskId = document.querySelector('#input4').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 4pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
 
-$('#btn4').on('click', function(event) {
-    // event.preventDefault();
+//5pm
+$('#btn5').on('click', function(event) {
+    event.preventDefault();
     var task = document.querySelector('#input5').value;
     localStorage.setItem('task8', task);
     var taskId = document.querySelector('#input5').value;
-    $('#saveAlert').text('Appointment added to local storage');
+    $('#saveAlert').text('Task saved for 5pm');
+    $('input[type="text"]').val('');
+    lastRegisteredTasks();
 });
+//End of on click events
