92d161d77477d08776f5b3026a0f01ed9a1dc5e6
Angel Flores <angelfloreschicago@gmail.com>
2021-04-10 12:07:34 -0500

    a few updates to js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,78 @@
-var savebtn = document.getElementsByName('name')
-var task9 = document.querySelector('#input9');
-var task10 = document.querySelector('#input10');
-var task11 = document.querySelector('#input11');
-var task12 = document.querySelector('#input12');
-var task1 = document.querySelector('#input1');
-var task2 = document.querySelector('#input2');
-var task3 = document.querySelector('#input3');
-var task4 = document.querySelector('#input4');
-var task5 = document.querySelector('#input5');
-
-// var savePrompt = $('#saveAlert');
+// var taskId = document.querySelector('#input9');
+///var savePrompt = $('#saveAlert');
 
 var today = moment();
 $("#currentDay").text(today.format("dddd MMM Do, YYYY"));
 
-savebtn.addEventListener('click', function(event) {
-    event.preventDefault();
 
-    var task = [{
-        am9: task9.value.trim(),
-        am10: task10.value.trim(),
-        am11: task11.value.trim(),
-        am12: task12.value.trim(),
-        am1: task1.value.trim(),
-        am2: task2.value.trim(),
-        am3: task3.value.trim(),
-        am4: task4.value.trim(),
-        am5: task5.value.trim(),
-    }]
-    localStorage.setItem('task', JSON.stringify(task));
+$('#btn9').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input9').value;
+    localStorage.setItem('task', task);
+    var taskId = document.querySelector('#input9').value;
     $('#saveAlert').text('Appointment added to local storage');
 });
 
+$('#btn10').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input10').value;
+    localStorage.setItem('task1', task);
+    var taskId = document.querySelector('#input10').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
 
-// var task = document.querySelector('#input9').value;
-//     localStorage.setItem('task', task);
-//     var taskId = document.querySelector('#input9').value;
+$('#btn11').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input11').value;
+    localStorage.setItem('task2', task);
+    var taskId = document.querySelector('#input11').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn12').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input12').value;
+    localStorage.setItem('task3', task);
+    var taskId = document.querySelector('#input12').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn1').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input1').value;
+    localStorage.setItem('task4', task);
+    var taskId = document.querySelector('#input1').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn2').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input2').value;
+    localStorage.setItem('task5', task);
+    var taskId = document.querySelector('#input2').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn3').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input3').value;
+    localStorage.setItem('task6', task);
+    var taskId = document.querySelector('#input3').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn4').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input4').value;
+    localStorage.setItem('task7', task);
+    var taskId = document.querySelector('#input4').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
+
+$('#btn4').on('click', function(event) {
+    // event.preventDefault();
+    var task = document.querySelector('#input5').value;
+    localStorage.setItem('task8', task);
+    var taskId = document.querySelector('#input5').value;
+    $('#saveAlert').text('Appointment added to local storage');
+});
